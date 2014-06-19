f2795636e56c4abcee21c07c1842265e88030190
Rodrigo Chaves <rodrichaves2010@gmail.com>
2014-06-19 15:22:58 -0300

    First Commit

---
+++ app/src/main/java/br/rodricxc/Maps3/app/MainActivity.java
@@ -0,0 +1,51 @@
+package br.rodricxc.Maps3.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button buttonHistorico = (Button) findViewById(R.id.button_historico);
+
+        buttonHistorico.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goHistorico();
+            }
+        });
+
+        Button buttonMeuLocal = (Button) findViewById(R.id.button_meulocal);
+
+        buttonMeuLocal.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goMyPlace();
+            }
+        });
+
+
+
+
+    }
+
+    public void goHistorico() {
+        Intent i  = new Intent(this, MapsActivity.class);
+        startActivity(new Intent(this, MapsActivity.class));
+        return;
+    }
+
+
+    public void goMyPlace() {
+        Intent i  = new Intent(this, MeuLocal.class);
+        startActivity(new Intent(this, MeuLocal.class));
+        return;
+    }
+}
---
+++ app/src/main/java/br/rodricxc/Maps3/app/MeuLocal.java
@@ -0,0 +1,59 @@
+package br.rodricxc.Maps3.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MeuLocal extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meu_local);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.meu_local, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
